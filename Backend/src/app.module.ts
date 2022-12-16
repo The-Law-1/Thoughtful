c7954a5d93d73b6f2e3b16afd74856a563286bf3
Gabriel <gabriel.griffin@epitech.eu>
2022-12-16 13:32:44 +0100

    App runs but can't connect to mongo

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CatsController } from "./cats/cats.controller";
 import { MongooseModule } from "@nestjs/mongoose";
+import { CatsService } from "./cats/cats.service";
+import { CatsModule } from "./cats/cats.module";
+import { CatSchema } from "./cats/schemas/cat.schema";
 
 @Module({
     // ! I have no clue if this URL is correct
-	imports: [MongooseModule.forRoot("mongodb://localhost/nest")],
-	controllers: [AppController, CatsController],
+	imports: [MongooseModule.forRoot("mongodb://root:root@localhost:27017/thoughtful"), CatsModule],
+	controllers: [AppController],
 	providers: [AppService],
 })
 export class AppModule {}
--- Backend/src/cats/cats.module.ts
+++ Backend/src/cats/cats.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { CatsController } from "./cats.controller";
-// import { CatsService } from "./cats/cats.service";
+import { CatsService } from "./cats.service";
 import { Cat, CatSchema } from "./schemas/cat.schema";
 
 // * we make a module for cats to store all the stuff and export it as a module
 @Module({
     imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
     controllers: [CatsController],
-    providers: [],
+    providers: [CatsService],
 })
 
 export class CatsModule {}
