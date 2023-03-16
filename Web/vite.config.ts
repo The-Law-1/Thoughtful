1d2ee1630eee0bb4c68dae872a63b45da2e769d0
Gabriel <gabriel.griffin@epitech.eu>
2023-03-16 19:40:08 +0100

    ✨ Static deployment take 4

--- Web/vite.config.ts
+++ Web/vite.config.ts
@@ -7,7 +7,7 @@ export default ({ mode }) => {
     process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
     return defineConfig({
-        base: "/thoughtful/",
+      base: "/Thoughtful/",
     
       plugins: [vue()],
       resolve: {
